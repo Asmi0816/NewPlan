3c84516011a0bdb86a671dbf04915042aa7e3c5e
Arick Smith <kidwriter2020@gmail.com>
2016-09-26 13:31:46 -0600

    Made a random color call.

--- NewPlan/ViewController.swift
+++ NewPlan/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var buttonEffect: UIButton!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClick(sender: UIButton) ->Void
+        
+    {
+       
+    }
+    private func makeRandomColor() -> UIColor
+    {
+        let randomColor : UIColor
+        //Color in Swift is a precentag - 0 - 1 inclusive.
+        //Of type CGFloat for allcomponents : RGBA
+        let redColor : CGFloat = CGFloat(drand48())
+        //  Red%
+        let greenColor : CGFloat = CGFloat (Double(arc4random_uniform(256))/(255.00))
+        // Green%
+        let blueColor : CGFloat = CGFloat (drand48())
+        //  Blue%
+        let alphaChannel : CGFloat = 1.0
+        // A%
+        randomColor = UIColor (red: redColor, green: greenColor, blue: blueColor, alpha: alphaChannel)
+        return randomColor
+    }
 
 }
 
